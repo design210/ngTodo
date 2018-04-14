3d0f27324ca18ed0e15c93fbec1f3214753f98de
heesungkang <khs@5411>
2018-04-14 11:16:16 +0900

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HomeComponent } from './home/home.component';
+import { JqueryComponent } from './jquery/jquery.component';
+import { AngularComponent } from './angular/angular.component';
+import {RouterModule, Routes} from '@angular/router';
+
+
+var loadChildren;
+const route: Routes =[
+  {path: '', component: IndexComponent}, //사용자 사이트
+  {path: 'admin', loadChildren: 'app/admin/admin.module#AdminModule'}, //관라지 사이트 , lazyLoading
+  
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    HomeComponent,
+    JqueryComponent,
+    AngularComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(route),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
