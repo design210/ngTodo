3a9f578bef4f84ee0813d10ccbc6a0549a78685a
heesungkang <khs@5411>
2018-04-14 13:23:56 +0900

    scss 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,20 @@ import { HomeComponent } from './home/home.component';
 import { JqueryComponent } from './jquery/jquery.component';
 import { AngularComponent } from './angular/angular.component';
 import {RouterModule, Routes} from '@angular/router';
+import {MatButtonModule, MatCardModule, MatIconModule, MatMenuModule, MatToolbar, MatToolbarModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 
 var loadChildren;
 const route: Routes =[
-  {path: '', component: IndexComponent}, //사용자 사이트
-  {path: 'admin', loadChildren: 'app/admin/admin.module#AdminModule'}, //관라지 사이트 , lazyLoading
-  
+  {path: '', component: IndexComponent, children:[
+      {path: '', component: HomeComponent},
+      {path: 'jquery', component:JqueryComponent},
+      {path: 'angular', component:AngularComponent},
+    ]}, //사용자 사이트
+ // {path: 'admin', loadChildren: 'app/admin/admin.module#AdminModule'}, //관라지 사이트 , lazyLoading
+
 ];
 
 @NgModule({
@@ -27,7 +34,15 @@ const route: Routes =[
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(route),
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
